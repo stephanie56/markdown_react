df064bacd7d19f49cb77ef9b7b3003a6cce75d56
Jon1701 <jon.j.balon@gmail.com>
2016-10-13 20:32:34 -0400

    Initial commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+  render() {
+
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// React.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// React Components.
+import HelloWorld from 'components/HelloWorld.jsx';
+
+// Add stylesheets.
+require("./stylesheets/stylesheet.scss");
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('react-target'));
