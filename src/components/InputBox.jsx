163febe0df2606a7727f3481c9cb5a3536668442
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-11-05 16:33:24 -0400

    Added handleChange function

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 export default class InputBox extends React.Component {
 
+  handleChange(e){
+    console.log(e.target.value)
+  }
+
   render(){
     return(
       <div>
-        <textarea />
+        <textarea onChange={this.handleChange.bind(this)}/>
 
       </div>
 
